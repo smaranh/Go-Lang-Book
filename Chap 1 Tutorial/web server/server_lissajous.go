7b4c0cb4c4d8977ca34ed9ad49abaa877b403a6e
Smaran Harihar <smaran.harihar@gmail.com>
2021-10-03 06:55:00 -0700

    display lissajous gif using server

---
+++ Chap 1 Tutorial/web server/server_lissajous.go
@@ -0,0 +1,59 @@
+// Server1 is a minimal "echo" server.
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+)
+
+var palette = []color.Color{color.White, color.RGBA{140, 140, 40, 255}}
+
+const (
+	whiteIndex = 0 // first color in palette
+	blackIndex = 1 // second color in palette
+)
+
+func main() {
+	http.HandleFunc("/", handler) // each request calls handler
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+// handler echoes the Path component of the request URL r.
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	lissajous(w)
+}
+
+func lissajous(out http.ResponseWriter) {
+	const (
+		cycles  = 5
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+}
